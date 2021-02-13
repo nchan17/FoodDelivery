e44908c36b629e9e8defbdfd33ccc92d4e1e7e56
Nino Chanturia <nchan17@freeuni.edu.ge>
2021-02-13 17:32:30 +0400

    minor fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,7 +39,7 @@ function loadCurrentWeather(data){
         <div style="float: left; width: 130px;">
         <div style="display: block; clear: left;">
             <div style="float: left;" title="Titel">
-                <img height="45" width="45" style="border: medium none; width: 45px; height: 45px; background: url(&quot;https://openweathermap.org/img/w/${data.weather[0].icon}.png&quot;) repeat scroll 0% 0% transparent;" alt="title" src="http://openweathermap.org/images/transparent.png"/>
+                <img height="45" width="45" style="border: medium none; width: 45px; height: 45px; background: url(&quot;https://openweathermap.org/img/w/${data.weather[0].icon}.png&quot;) repeat scroll 0% 0% transparent;" alt="title" src="https://openweathermap.org/images/transparent.png"/>
             </div>
             <div style="float: left;">
                 <div style="display: block; clear: left; font-size: medium; font-weight: bold; padding: 0pt 3pt;" title="Current Temperature">${data.main.temp} &deg;C</div>
@@ -71,7 +71,7 @@ function loadForecast(data){
             <div style="float: left; width: 130px;">
             <div style="display: block; clear: left;">
                 <div style="float: left;" title="Titel">
-                    <img height="45" width="45" style="border: medium none; width: 45px; height: 45px; background: url(&quot;https://openweathermap.org/img/w/${elem.weather[0].icon}.png&quot;) repeat scroll 0% 0% transparent;" alt="title" src="http://openweathermap.org/images/transparent.png"/>
+                    <img height="45" width="45" style="border: medium none; width: 45px; height: 45px; background: url(&quot;https://openweathermap.org/img/w/${elem.weather[0].icon}.png&quot;) repeat scroll 0% 0% transparent;" alt="title" src="https://openweathermap.org/images/transparent.png"/>
                 </div>
                 <div style="float: left;">
                     <div style="display: block; clear: left; font-size: medium; font-weight: bold; padding: 0pt 3pt;" title="Current Temperature">${elem.main.temp} &deg;C</div>
